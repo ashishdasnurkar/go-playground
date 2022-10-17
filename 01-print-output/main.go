576faca9e3f6236ae0f148f4d124a9992dfb72fa
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-10-17 21:15:50 +1100

    more prints

--- 01-print-output/main.go
+++ 01-print-output/main.go
@@ -5,6 +5,11 @@ import "fmt"
 func main() {
 
 	fmt.Println("Hello world!")
-	fmt.Println("Hello", "world", "!")
-	fmt.Print("Hello", "world", "!")
+	fmt.Println("Hello", "world", "!") // adds spaces and a new line
+	fmt.Print("Hello", "world", "!")   // no spaces and no new line
+	fmt.Println("")
+	fmt.Println(`multiline
+string
+is possible with backticks 
+in go`)
 }
