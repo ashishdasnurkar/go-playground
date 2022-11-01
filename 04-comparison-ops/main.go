a7072e266ac4b1ab2252b03e04a5ed9c88af6275
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2022-11-01 23:18:44 +1100

    str compare example

--- 04-comparison-ops/main.go
+++ 04-comparison-ops/main.go
@@ -1,9 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	num := 42
+	r := bufio.NewReader(os.Stdin)
+	line, err := r.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+
+	if strings.TrimSpace(line) == "hi" {
+		fmt.Println("hi back..")
+	} else {
+		fmt.Println("whatever")
+	}
+
 	if num == 42 {
 		fmt.Println("num is 42")
 	}
